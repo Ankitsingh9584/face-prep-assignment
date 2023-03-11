5cce7eb0c6ccaf2b6c19caf33331fcf6ab22dcdb
unknown <ankits.8080@gmail.com>
2023-03-11 12:51:14 +0530

    added

--- faceprep/src/components/dashboard.jsx
+++ faceprep/src/components/dashboard.jsx
@@ -1,4 +1,4 @@
-import { Box, Text} from "@chakra-ui/react";
+import { Box, Button, Flex, Text} from "@chakra-ui/react";
 import { color } from "framer-motion";
 import { useNavigate } from "react-router-dom";
 
@@ -12,9 +12,9 @@ navigate("/")
 
     return(
         <>
-       <Box w={"full"} height={"55px"} bgColor={"blue"}>
-   <Text onClick={callLogout} _hover={{color:"orange" , cursor:"pointer"} } fontWeight={"bold"} fontSize={"2xl"} color={"white"} align={"end"} paddingRight={'5%'}>Logout</Text>
-       </Box>
+       <Flex w={"full"} height={"55px"} bgColor={"blue"} justify={"flex-end"}>
+   <Text w={"10%"}  onClick={callLogout} _hover={{color:"orange" , cursor:"pointer"} } fontWeight={"bold"} fontSize={"2xl"} color={"white"} align={"end"} paddingRight={'5%'}>Logout</Text>
+       </Flex>
         </>
     )
 }
